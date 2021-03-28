cd99469689cedec202a7c9af41c2e555c5716559
Dikson Rodrigues <eoscreativetech@gmai.com>
2021-03-28 11:49:34 -0300

    push tirada

--- App.js
+++ App.js
@@ -3,34 +3,12 @@ import { StatusBar } from "expo-status-bar";
 import React, { useEffect } from "react";
 import { AppLoading } from "expo";
 import { useFonts, Anton_400Regular } from "@expo-google-fonts/anton";
-import * as Notifications from 'expo-notifications';
-import * as Permissions from 'expo-permissions';
 
 import Routes from "./src/router";
 
 
 
 export default function App() {
-
-  useEffect(()=>{
-    registerForPushNotifications().then(token => console.log(token)).
-    catch(err => console.log(Err))
-  }, [])
-
-
-
-  async function registerForPushNotifications(){
-    const {status} = await Permissions.getAsync(Permissions.NOTIFICATIONS);
-     if (status!='granted') {
-       const {status} = await Permissions.askAsync(Permissions.NOTIFICATIONS)
-     }
-     if (status !='granted') {
-       alert('Falha em obter o token push');
-       return;
-     }
-     token = (await Notifications.getExpoPushTokenAsync()).data;
-     return token;
-  }
   
   console.disableYellowBox = true;
 
